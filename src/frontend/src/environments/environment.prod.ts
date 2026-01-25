15bdafbd7dc6dc603b920e757e58d525386cfd4e
samlinux <rbole@samlinux.at>
2026-01-25 10:10:31 +0100

    improve frontend and Angular components

--- src/frontend/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-// Canister IDs and configuration for production
-export const environment = {
-  production: true,
-  backendCanisterId: 'nn3mi-waaaa-aaaam-afw7q-cai',
-  backendAuthKey: 'NyBNmoXX32DzpTd3UGxi'
-};
--- src/frontend/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-// Canister IDs and configuration
-export const environment = {
-  production: false,
-  backendCanisterId: 'uxrrr-q7777-77774-qaaaq-cai',
-  backendAuthKey: 'NyBNmoXX32DzpTd3UGxi'
-};
